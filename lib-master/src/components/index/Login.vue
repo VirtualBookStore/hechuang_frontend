d4c7ab5e0f00ab0191ae7ad52ac2f5cebd4674f2
Fibonaccishen <49826339+Fibonaccishen@users.noreply.github.com>
2020-12-20 19:50:42 +0800

    frontend

---
+++ lib-master/src/components/index/Login.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="login_container">
+    <div class="libtip">
+      <h1>
+        合创书店
+        <i class="iconfont icon-shudian1"></i>
+      </h1>
+      <p>合创书店，一个为书籍爱好者设计的集购买、回收于一体的网上书店。</p>
+    </div>
+    <transition name="form-fade" mode="in-out">
+      <section class="formCon">
+        <el-form
+          ref="loginFormRef"
+          :model="loginForm"
+          :rules="loginFormRules"
+          label-width="100px"
+        >
+          <el-form-item prop="username" label="用户ID">
+            <el-input v-model="loginForm.username" placeholder="ID"></el-input>
+          </el-form-item>
+          <el-form-item prop="password" label="密码">
+            <el-input
+              type="password"
+              v-model="loginForm.password"
+              placeholder="密码"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button>忘记密码</el-button>
+          </el-form-item>
+        </el-form>
+      </section>
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      //登录表单数据绑定
+      loginForm: {
+        username: '',
+        password: '',
+      },
+      //表单的验证规则
+      loginFormRules: {
+        //    验证用户名是否合法
+        username: [
+          { required: true, message: '请输入ID', trigger: 'blur' },
+          {
+            min: 0,
+            max: 100,
+            message: '登录凭据必须为ID或11位手机号',
+            trigger: 'blur',
+          },
+        ],
+        //    验证密码是否合法
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          {
+            min: 6,
+            max: 15,
+            message: '密码必须在6-15个字符之间',
+            trigger: 'blur',
+          },
+        ],
+      },
+    }
+  },
+  methods: {
+    //异步操作
+    login() {
+      this.$router.push({ path: '/Admin' })
+    },
+  },
+}
+</script>
+<style scoped>
+.login_container {
+  height: 100%;
+  width: 100%;
+  /* background-image: url(../../assets/img/bg1.jpg); */
+  background-size: 100%;
+  background-attachment: fixed;
+}
+
+.formCon {
+  width: 320px;
+  height: 190px;
+  position: absolute;
+  top: 36%;
+  left: 36%;
+
+  /* margin-top: 160px;
+		margin-left: 95px; */
+  /* .wh(320px, 190px);
+		.ctp(320px, 190px); */
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #fdffff;
+}
+.libtip {
+  position: absolute;
+  width: 100%;
+  padding-top: 50px;
+  text-align: center;
+}
+.libtip h1 {
+  font-size: 34px;
+  margin: 0;
+  line-height: 48px;
+  color: #555;
+}
+.libtip p {
+  font-size: 18px;
+  line-height: 28px;
+  color: #888;
+  margin: 10px 0 5px;
+}
+
+.form-fade-enter-active,
+.form-fade-leave-active {
+  transition: all 1s;
+}
+.form-fade-enter,
+.form-fade-leave-active {
+  transform: translate3d(0, -50px, 0);
+  opacity: 0;
+}
+</style>
---
+++ lib-master/src/views/Index.vue
@@ -0,0 +1,85 @@
+<template>
+  <el-container class="index-container">
+    <!--        上边框-->
+    <el-header class="elheader">
+      <el-container class="mainArea">
+        <!--            开启了路由模式-->
+        <el-menu
+          router
+          default-active="/login"
+          class="elmenu"
+          mode="horizontal"
+        >
+          <el-menu-item index="/login">登录</el-menu-item>
+          <el-menu-item index="/studentRegister">注册</el-menu-item>
+        </el-menu>
+      </el-container>
+    </el-header>
+
+    <!--        主体区域-->
+    <el-main class="elmain">
+      <!--            路由占位符-->
+      <router-view></router-view>
+    </el-main>
+    <el-footer class="elfooter">
+      <div class="bottomNav">
+        <a href="#" class="border-right">意见反馈</a>
+        <a href="#" class="border-right">联系我们</a>
+        <a href="#" class="border-right">投诉举报</a>
+      </div>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+@import url('//at.alicdn.com/t/font_2280831_jxhnjmjk6m.css');
+.index-container {
+  height: 100%;
+}
+.elheader {
+  margin: auto;
+  width: 60%;
+  border-bottom: 1px solid #e7e7e7;
+}
+.mainArea {
+  margin: auto;
+}
+.elmenu {
+  /* border-radius: 2px; */
+  list-style: none;
+  position: relative;
+  padding-left: 0;
+}
+
+.elmain {
+  background-color: rgb(255, 255, 255);
+  padding-top: 1px;
+  padding: 0px;
+}
+.elfooter {
+  margin: auto;
+  width: 60%;
+  border-top: 1px solid #e7e7e7;
+}
+.bottomNav {
+  margin-top: 10px;
+  text-align: center;
+}
+.border-right {
+  clear: both;
+  text-align: center;
+  color: #999;
+  font: 12px/1.5 Arial;
+  white-space: nowrap;
+  border-right: 1px solid #d8d8d8;
+  padding: 0 10px;
+}
+</style>
