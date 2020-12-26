487013a11778357590e976c6b151d31585f2c5dd
YukikazeSama <51656908+YukikazeSama@users.noreply.github.com>
2020-12-26 16:38:12 +0800

    更新

--- lib-master/src/components/searchbook/AllBook.vue
+++ lib-master/src/components/searchbook/AllBook.vue
@@ -1,133 +1,191 @@
 <template>
-  <div class="book_search">
-    <el-row style="margin-top: 20px;">
-      <el-col :span="14" :offset="4">
-        <header class="form_header"></header>
-        <el-form
-          ref="bookFormRef"
-          :model="bookForm"
-          :rules="bookFormRules"
-          label-width="110px"
-          class="demo-now_Data"
-        >
-          <!-- <el-form-item label="读者ID" prop="reader_id">
-            <el-input v-model="bookForm.reader_id" placeholder="读者ID"></el-input>
-          </el-form-item> -->
-          <el-button type="primary" @click="search" class="submit_btn">点击获得所有图书信息</el-button>
-        </el-form>
-      </el-col>
-    </el-row>
-    <el-dialog title="所有图书" :visible.sync="searchdialogvisible" width="50%">
-        <el-table :data="searchlist" style="width: 100%" border :stripe="true">
-            <el-table-column prop="book_name" label="书名" width="120"></el-table-column>
-            <el-table-column prop="id" label="馆藏ID"></el-table-column>
-            <el-table-column prop="place_id" label="位置ID"></el-table-column>
-            <el-table-column prop="publishing_house" label="出版社"></el-table-column>
-            <el-table-column prop="introduction" label="简介"></el-table-column>
-            <el-table-column prop="publication_date" label="出版时间"></el-table-column>
-            <el-table-column prop="isbn" label="ISBN"></el-table-column>
-            <el-table-column prop="author" label="作者"></el-table-column>
-            <el-table-column prop="state" label="借还状态"></el-table-column>   
-        </el-table>
-        <!-- 底部按钮区域 -->
-        <span slot="footer" class="dialog-footer">
-            <el-button @click="searchdialogvisible=false">确定</el-button>
-
-        </span>
+  <div class="basetable"
+       v-loading="loading"
+       element-loading-text="拼命加载中">
+    <!-- v-loading 设置加载 -->
+    <div class="selectMenu">
+      <!-- 点击触发add方法 -->
+      <el-button type="primary"
+                 @click="add">新增</el-button>
+    </div>
+    <div class="tableMain">
+      <el-table :data="tableData"
+                style="width: 100%">
+        <el-table-column prop="isbn"
+                         label="isbn"></el-table-column>
+        <el-table-column prop="title"
+                         label="书名"></el-table-column>
+        <el-table-column prop="description"
+                         label="描述"></el-table-column>
+        <el-table-column prop="price"
+                         label="价格"></el-table-column>
+        <el-table-column prop="new_total"
+                         label="新书库存"></el-table-column>
+        <el-table-column prop="old_total"
+                         label="旧书库存"></el-table-column>
+        <el-table-column prop="recommended"
+                         label="是否被推荐"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <!-- 点击编辑进入编辑页面进行编辑表格数据 -->
+            <el-button size="small"
+                       @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="small"
+                       type="danger"
+                       @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="page">
+      <el-pagination @size-change="handleSizeChange"
+                     @current-change="handleCurrentChange"
+                     :current-page.sync="currentPage3"
+                     :page-size="100"
+                     layout="prev, pager, next, jumper"
+                     :total="1000"></el-pagination>
+    </div>
+    <!-- 下面这个用来设置点击添加按钮的弹出框，里面可以进行嵌套表格来展示弹出的表格信息,使用下面的:visible.sync来控制显示与否 -->
+    <!-- 里面绑定的是我们新设置的值，填写完成后，将我们这个新值塞到页面中所有的数据当中去 -->
+    <el-dialog title="用户信息"
+               :visible.sync="dialogFormVisible">
+      <!-- 在el-dialog中进行嵌套el-form实现弹出表格的效果 -->
+      <el-form :model="form">
+        <el-form-item label="描述"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.description"
+                    auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="价格"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.price"
+                    auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="新书库存"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.new_total"
+                    auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="二手库存"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.old_total"
+                    auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="推荐状态"
+                      :label-width="formLabelWidth">
+          <el-input v-model="form.recommended"
+                    auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button @click="cancel">取 消</el-button>
+        <!-- 设置触发更新的方法 -->
+        <el-button type="primary"
+                   @click="update">确 定</el-button>
+      </div>
     </el-dialog>
   </div>
 </template>
 
-<script>
-    export default {
-        data() {
-            return {
-                searchlist:[],
-                searchdialogvisible:false,
-                //图书表单数据绑定
-                bookForm: {
-                    reader_id: '',
-                },
-                //表单的验证规则
-                bookFormRules: {
-                    //    验证书名是否合法
-                    reader_id: [
-                        { required: true, message: "请输入读者ID", trigger: "blur" },
-                    ],
-                }
-            }
-        },
-        methods: {
-            //异步操作
-            search() {
-                this.$refs.bookFormRef.validate(async valid => {
-                    if (!valid) return;
-                    let msg = "";
-                    let status = 200; 
-                    let key = this.bookForm.reader_id; 
-                    let Token = window.sessionStorage.getItem('token'); //this.$message.info("The database isn't ready.");
-                    let result = await this.$http.get('/apip/api/books', {
-                        hearders: {
-                            'Authorization': Token
-                        },
-                    })
-                        .catch(function (error) {
-                            if (error.response) {
-                                status = error.response.status;
-                                msg = error.response.data.msg;
-                            }
-                        });
-                        this.searchlist=result.data
-                    if (status === 400) {
-                        this.$message.info("该人不存在 !");
-                    }
-                });
-
-                this.searchdialogvisible=true
-
-
-            }
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+      loading: true,
+      //   表格的数据
+      tableData: [
+        {
+          isbn: "士兵76",
+          title: "男",
+          description: "国王大道",
+          price: "500",
+          new_total: "5",
+          old_total: "5",
+          recommended: "false"
         }
+      ],
+      dialogFormVisible: false,
+      formLabelWidth: "80px",
+      // 设置form用于进行添加的时候绑定值
+      form: {},
+      value6: "",
+      currentPage3: 1,
+      currentIndex: ""
+    };
+  },
+  created () {
+    //   设置回调函数，进行1.5秒的loading动画显示
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  },
+  methods: {
+    showTime () {
+      this.$alert(this.value6, "起止时间", {
+        confirmButtonText: "确定",
+        callback: action => {
+          this.$message({
+            type: "info",
+            message: "已显示"
+          });
+        }
+      });
+    },
+    // 增加数据的方式，单独的设置一些值，用于增加功能，这些值放在对象里面进行设置，然后将这个新增的对象塞到总数据里面
+    add () {
+      this.form = {
+
+      };
+      //   设置点击按钮之后进行显示对话框
+      this.dialogFormVisible = true;
+    },
+    update () {
+      //   this.form.date = reformat(this.form.date);
+      //    可以在html上面进行设置日期的格式化
+      //   将我们添加的信息提交到总数据里面
+      this.tableData.push(this.form);
+      this.dialogFormVisible = false;
+    },
+    handleEdit (index, row) {
+      // 将数据的index传递过来用于实现数据的回显
+      this.form = this.tableData[index];
+      this.currentIndex = index;
+      // 设置对话框的可见
+      this.dialogFormVisible = true;
+    },
+    handleDelete (index, row) {
+      // 设置类似于console类型的功能
+      this.$confirm("永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          // 移除对应索引位置的数据，可以对row进行设置向后台请求删除数据
+          this.tableData.splice(index, 1);
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    cancel () {
+      // 取消的时候直接设置对话框不可见即可
+      this.dialogFormVisible = false;
+    },
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`);
     }
+  }
+};
 </script>
-
-<style scoped>
-    .home-container {
-        height: 100%;
-    }
-	.submit_btn {
-		left:90%;
-	}
-
-    .el-header {
-        background-color: rgb(64,158,255);
-        display: flex;
-        justify-content: space-between;
-        margin-left: 0;
-        padding-left: 0;
-
-    }
-
-    .el-aside {
-        /*background-color: gray;*/
-    }
-
-    .el-main {
-        /*background-color: lightgoldenrodyellow;*/
-    }
-
-    .out-button{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-
-    .header-name{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-    }
-
-</style>
